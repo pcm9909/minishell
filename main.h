b60fe7358ada85b650418f517bbda30134314371
pcm9909 <pcm9909@naver.com>
2024-07-26 21:36:41 +0900

    [Chore] 구조체 멤버 변경

--- main.h
+++ main.h
@@ -1,33 +1,20 @@
 #include "libft/libft.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <fcntl.h>
 
-typedef struct s_node
-{
-	char *content;
-	struct s_node *next;
-	struct s_node *prev;
-}				t_node;
-
-
-typedef struct s_queue
-{
-	t_node *head;
-	t_node *tail;
-}				t_queue;
 
 typedef struct s_command
 {
-	struct t_queue *queue;
-	bool exist;
+	char	**command;
 }				t_command;
 
 typedef struct s_redirection
 {
-	struct s_command *double_left_brace;
-	struct s_command *left_brace;
-	struct s_command *command;
-	struct s_command *right_brace;
-	struct s_command *double_right_brace;
+	struct s_command	*double_left_brace;
+	struct s_command	*left_brace;
+	struct s_command	*command;
+	struct s_command	*right_brace;
+	struct s_command	*double_right_brace;
 }				t_redirection;
 
