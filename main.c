53cd538a74b4b9fabe3bdcc9b06e91d0a38ff5ce
pcm9909 <pcm9909@naver.com>
2024-07-26 23:39:36 +0900

    [fix] add_command leak 발생지점 수정

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int ft_isspace(int c)
 {
-    if((c >= 9 && c <= 13) || c == 32)
+    if ((c >= 9 && c <= 13) || c == 32)
         return 1;
     return 0;
 }
@@ -35,7 +35,8 @@ char **add_command(char ***cmd, char *str)
     if (*cmd == NULL)
     {
         new_cmd = malloc(sizeof(char *) * 2);
-        if (new_cmd == NULL) return NULL;
+        if (new_cmd == NULL)
+            return NULL;
         new_cmd[0] = ft_strdup(str);
         new_cmd[1] = NULL;
         return new_cmd;
@@ -43,11 +44,15 @@ char **add_command(char ***cmd, char *str)
     while ((*cmd)[i])
         i++;
     new_cmd = malloc(sizeof(char *) * (i + 2));
-    if (new_cmd == NULL) return NULL;
+    if (new_cmd == NULL)
+        return NULL;
     for (int j = 0; j < i; j++)
         new_cmd[j] = ft_strdup((*cmd)[j]);
     new_cmd[i] = ft_strdup(str);
     new_cmd[i + 1] = NULL;
+
+    for (int j = 0; j < i; j++)
+        free((*cmd)[j]);
     free(*cmd);
 
     return new_cmd;
@@ -58,21 +63,21 @@ void ft_free1(char ***command)
     int i;
 
     i = 0;
-    if((*command))
+    if ((*command))
     {
-        while((*command)[i])
+        while ((*command)[i])
         {
             free((*command)[i]);
             i++;
         }
     }
-	*command = NULL;
-	free(*command);
+    *command = NULL;
+    free(*command);
 }
 
 int main()
 {
-    char *str = "         \0";
+    char *str = "<ls < ls < a\0";
     t_redirection *command;
     char *content;
     init_redirection(&command);
@@ -80,43 +85,43 @@ int main()
     int flag = 0;
     int j;
 
-	if(str == NULL)
-		exit(0);
-    while(str[i])
+    if (str == NULL)
+        exit(0);
+    while (str[i])
     {
-		flag = 0;
-        while(ft_isspace(str[i]))
+        flag = 0;
+        while (ft_isspace(str[i]))
             i++;
-        if(str[i] == '<')
+        if (str[i] == '<')
         {
             i++;
-            if(str[i] && str[i] == '<')
+            if (str[i] && str[i] == '<')
             {
                 i++;
                 flag = 1;
             }
-            while(ft_isspace(str[i]))
+            while (ft_isspace(str[i]))
                 i++;
             j = i;
             while (str[i] && !ft_isspace(str[i]) && str[i] != '>' && str[i] != '<')
                 i++;
             content = ft_substr(str, j, i - j);
-            if(flag == 1)
+            if (flag == 1)
             {
-                command->double_left_brace->command = add_command(&command->double_left_brace->command ,content);
+                command->double_left_brace->command = add_command(&command->double_left_brace->command, content);
             }
-			else
-				command->left_brace->command = add_command(&command->left_brace->command ,content);
+            else
+                command->left_brace->command = add_command(&command->left_brace->command, content);
         }
-        else if(str[i] == '>')
+        else if (str[i] == '>')
         {
             i++;
-            if(str[i] && str[i] == '>')
+            if (str[i] && str[i] == '>')
             {
                 i++;
                 flag = 1;
             }
-            while(ft_isspace(str[i]))
+            while (ft_isspace(str[i]))
                 i++;
             j = i;
             while (str[i] && !ft_isspace(str[i]) && str[i] != '>' && str[i] != '<')
@@ -133,28 +138,28 @@ int main()
         free(content);
         i++;
     }
-	i = 0;
-	while(command->left_brace->command[i])
-	{
-		int fd = open(command->left_brace->command[i], O_WRONLY);
-		{
-			if(fd == -1)
-			{
-				perror(command->left_brace->command[i]);
-				exit(1);
-			}
-		}
-		i++;
-	}
+    i = 0;
+    while (command->left_brace->command && command->left_brace->command[i])
+    {
+        int fd = open(command->left_brace->command[i], O_WRONLY);
+        {
+            if (fd == -1)
+            {
+                perror(command->left_brace->command[i]);
+                exit(1);
+            }
+        }
+        i++;
+    }
     ft_free1(&command->double_left_brace->command);
     ft_free1(&command->left_brace->command);
     ft_free1(&command->double_right_brace->command);
     ft_free1(&command->right_brace->command);
     ft_free1(&command->command->command);
     free(command->double_left_brace);
-	free(command->left_brace);
-	free(command->command);
-	free(command->double_right_brace);
-	free(command->right_brace);
-	free(command);
+    free(command->left_brace);
+    free(command->command);
+    free(command->double_right_brace);
+    free(command->right_brace);
+    free(command);
 }
