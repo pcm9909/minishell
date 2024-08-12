89923672c51661d7152a03cab6ebbfd1f16ca4b0
pcm9909 <pcm9909@naver.com>
2024-08-12 19:39:38 +0900

    [feat] add pipe dup2 fork

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 char *local;
 
 int is_whitespace(int c)
@@ -17,8 +16,8 @@ t_command *create_command(void)
         exit(EXIT_FAILURE);
     }
     cmd->command = NULL;
-	cmd->order = false;
-	cmd->exist = false;
+    cmd->order = false;
+    cmd->exist = false;
     return cmd;
 }
 
@@ -37,7 +36,6 @@ void initialize_redirection(t_redirection **redirection)
     (*redirection)->right_brace = create_command();
 }
 
-//realloc
 char **allocate_and_copy(char **cmd, int size)
 {
     char **new_cmd = malloc(sizeof(char *) * size);
@@ -133,23 +131,22 @@ void parse_left_redirection(const char *str, int *i, t_redirection *command)
     j = *i;
     if (str[*i] == '>' || str[*i] == '<' || str[*i] == '\0' || str[*i] == '|' || str[*i] == '&' || str[*i] == ';')
     {
-        //에러 출력부 변경필요!
         printf("zsh: parse error near `%c'\n", str[*i]);
         exit(258);
     }
-	while (str[*i] && !is_whitespace(str[*i]) && str[*i] != '>' && str[*i] != '<')
+    while (str[*i] && !is_whitespace(str[*i]) && str[*i] != '>' && str[*i] != '<')
         (*i)++;
-	content = ft_substr(str, j, *i - j);
+    content = ft_substr(str, j, *i - j);
     if (flag == 1)
-	{
-		command->double_left_brace->exist = true;
+    {
+        command->double_left_brace->exist = true;
         command->double_left_brace->command = append_command(&command->double_left_brace->command, content);
-	}
+    }
     else
-	{
-		command->left_brace->exist = true;
+    {
+        command->left_brace->exist = true;
         command->left_brace->command = append_command(&command->left_brace->command, content);
-	}
+    }
     free(content);
 }
 
@@ -168,7 +165,6 @@ void parse_right_redirection(char *str, int *i, t_redirection *command)
         (*i)++;
     if (str[*i] == '>' || str[*i] == '<' || str[*i] == '\0' || str[*i] == '|' || str[*i] == '&' || str[*i] == ';')
     {
-        //에러 출력부 변경필요!
         printf("zsh: parse error near `%c'\n", str[*i]);
         exit(258);
     }
@@ -177,15 +173,15 @@ void parse_right_redirection(char *str, int *i, t_redirection *command)
         (*i)++;
     content = ft_substr(str, j, *i - j);
     if (flag == 1)
-	{
-		command->double_right_brace->exist = true;
+    {
+        command->double_right_brace->exist = true;
         command->double_right_brace->command = append_command(&command->double_right_brace->command, content);
-	}
+    }
     else
-	{
-		command->right_brace->exist = true;
+    {
+        command->right_brace->exist = true;
         command->right_brace->command = append_command(&command->right_brace->command, content);
-	}
+    }
     free(content);
 }
 
@@ -199,8 +195,8 @@ void parse_command(char *str, int *i, t_redirection *command)
     j = *i;
     while (str[*i] && !is_whitespace(str[*i]) && str[*i] != '>' && str[*i] != '<')
         (*i)++;
-	if(j == *i)
-		return;
+    if (j == *i)
+        return;
     content = ft_substr(str, j, *i - j);
     command->command->command = append_command(&command->command->command, content);
     free(content);
@@ -208,53 +204,53 @@ void parse_command(char *str, int *i, t_redirection *command)
 
 char *ft_find_single_redirect(char *str, char c)
 {
-	int i = 0;
+    int i = 0;
 
-	if(!str)
-		return NULL;
-	while(str[i])
-	{
-		if(str[i] == c)
-		{
-			if(str[i + 1] == c)
-			{
-				i++;
-			}
-			else
-			{
-				return &str[i];
-			}
-		}
-		i++;
-	}
-	return NULL;
+    if (!str)
+        return NULL;
+    while (str[i])
+    {
+        if (str[i] == c)
+        {
+            if (str[i + 1] == c)
+            {
+                i++;
+            }
+            else
+            {
+                return &str[i];
+            }
+        }
+        i++;
+    }
+    return NULL;
 }
 
 void set_order(t_redirection *command, char *str)
 {
-	int dual;
-	int single;
+    int dual;
+    int single;
 
-	dual = ft_strlen(ft_strnstr(str, "<<", sizeof(str)));
-	single = ft_strlen(ft_find_single_redirect(str, '<'));
-	if(dual > single)
-	{
-		command->double_left_brace->order = true;
-	}
-	else if (dual < single)
-	{
-		command->left_brace->order = true;
-	}
-	dual = ft_strlen(ft_strnstr(str, ">>", ft_strlen(str)));
-	single = ft_strlen(ft_find_single_redirect(str, '>'));
-	if(dual > single)
-	{
-		command->double_right_brace->order = true;
-	}
-	else if (dual < single)
-	{
-		command->right_brace->order = true;
-	}
+    dual = ft_strlen(ft_strnstr(str, "<<", sizeof(str)));
+    single = ft_strlen(ft_find_single_redirect(str, '<'));
+    if (dual > single)
+    {
+        command->double_left_brace->order = true;
+    }
+    else if (dual < single)
+    {
+        command->left_brace->order = true;
+    }
+    dual = ft_strlen(ft_strnstr(str, ">>", ft_strlen(str)));
+    single = ft_strlen(ft_find_single_redirect(str, '>'));
+    if (dual > single)
+    {
+        command->double_right_brace->order = true;
+    }
+    else if (dual < single)
+    {
+        command->right_brace->order = true;
+    }
 }
 
 void parse_redirection(char *str, t_redirection *command)
@@ -278,187 +274,191 @@ void parse_redirection(char *str, t_redirection *command)
             parse_command(str, &i, command);
         }
     }
-	set_order(command, str);
+    set_order(command, str);
 }
-// <<
-void open_redirection_files0(t_redirection *command, int pipe_fd[])
+
+void open_redirection_files(t_redirection *command, int pipe_fd[])
 {
     int i = 0;
-	int cnt;
-	char *str = ft_strdup("");
+    int fd;
 
+    // Handle double left brace (<<)
     while (command->double_left_brace->command && command->double_left_brace->command[i])
     {
-		while(1)
-		{
-			local = readline(">");
-			if(ft_strlen(local) == ft_strlen(command->double_left_brace->command[i]) && !ft_strncmp(local, command->double_left_brace->command[i], sizeof(local)))
-			{
-				if(command->double_left_brace->order == true)
-				{
-					dup2(pipe_fd[1],1);
-					close(pipe_fd[0]);
-				}
-				write(pipe_fd[1], str, ft_strlen(str));
-				free(local);
-				break;
-			}
-			if(command->double_left_brace->order)
-			{
-				str = ft_strjoin(str,local);
-			}
-			free(local);
-		}
+        char *str = ft_strdup("");
+        while (1)
+        {
+            local = readline(">");
+            if (ft_strlen(local) == ft_strlen(command->double_left_brace->command[i]) && !ft_strncmp(local, command->double_left_brace->command[i], sizeof(local)))
+            {
+                if (command->double_left_brace->order == true)
+                {
+                    dup2(pipe_fd[1], 1);
+                    close(pipe_fd[0]);
+                }
+                write(pipe_fd[1], str, ft_strlen(str));
+                free(local);
+                break;
+            }
+            if (command->double_left_brace->order)
+            {
+                str = ft_strjoin(str, local);
+            }
+            free(local);
+        }
         i++;
     }
-}
 
-
-// <
-void open_redirection_files1(t_redirection *command, int *fd, int pipe_fd[])
-{
-    int i = 0;
+    // Handle left brace (<)
+    i = 0;
     while (command->left_brace->command && command->left_brace->command[i])
     {
-        *fd = open(command->left_brace->command[i], O_RDONLY);
-        if (*fd == -1)
+        fd = open(command->left_brace->command[i], O_RDONLY);
+        if (fd == -1)
         {
             perror(command->left_brace->command[i]);
             exit(EXIT_FAILURE);
         }
+        if (command->left_brace->order == true)
+        {
+            dup2(fd, 0);
+            dup2(pipe_fd[1], 1);
+            close(pipe_fd[0]);
+        }
+        close(fd);
         i++;
     }
-	if(command->left_brace->order == true)
-	{
-		dup2(*fd, 0);
-		dup2(pipe_fd[1], 1);
-		close(pipe_fd[0]);
-	}
-}
-// >
-void open_redirection_files2(t_redirection *command)
-{
-	int i = 0;
-	while (command->right_brace->command && command->right_brace->command[i])
+
+    // Handle right brace (>)
+    i = 0;
+    while (command->right_brace->command && command->right_brace->command[i])
     {
-        int fd = open(command->right_brace->command[i], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+        fd = open(command->right_brace->command[i], O_CREAT | O_TRUNC | O_WRONLY, 0644);
         if (fd == -1)
         {
             perror(command->right_brace->command[i]);
             exit(EXIT_FAILURE);
         }
+        if (command->right_brace->order == true)
+        {
+            dup2(fd, 1);
+        }
         close(fd);
         i++;
     }
-}
-// >>
-void open_redirection_files3(t_redirection *command)
-{
-	int i = 0;
-	while (command->double_right_brace->command && command->double_right_brace->command[i])
+
+    // Handle double right brace (>>)
+    i = 0;
+    while (command->double_right_brace->command && command->double_right_brace->command[i])
     {
-        int fd = open(command->double_right_brace->command[i], O_CREAT | O_APPEND | O_WRONLY, 0644);
+        fd = open(command->double_right_brace->command[i], O_CREAT | O_APPEND | O_WRONLY, 0644);
         if (fd == -1)
         {
             perror(command->double_right_brace->command[i]);
             exit(EXIT_FAILURE);
+        }
+        if (command->double_right_brace->order == true)
+        {
+            dup2(fd, 1);
         }
         close(fd);
         i++;
     }
 }
 
-
-
 int cnt_cmd(char **split)
 {
-	int i = 0;
+    int i = 0;
 
-	while(split[i])
-	{
-		i++;
-	}
-	return i;
+    while (split[i])
+    {
+        i++;
+    }
+    return i;
 }
-
 
 void exe(t_redirection *command, char **cmd, char **envp, int pipe_fd[])
 {
-	char *cmd_path = NULL;
-	char *path = get_path(envp);
-	dup2(pipe_fd[0], 0);
-	close(pipe_fd[1]);
-	if(cmd)
-	{
-		cmd_path = get_cmd_path(cmd[0], path);
-	}
-	execve(cmd_path, cmd, NULL);
-	free(path);
+    char *cmd_path = NULL;
+    char *path = get_path(envp);
+    dup2(pipe_fd[0], 0);
+    close(pipe_fd[1]);
+    if (cmd)
+    {
+        cmd_path = get_cmd_path(cmd[0], path);
+    }
+    execve(cmd_path, cmd, NULL);
+    free(path);
 }
 
-
-
-void execute_left_brace(t_redirection *command, int pipe_fd[], char **envp)
+void execute_command(t_redirection *command, char **envp)
 {
-	int	fd;
+    int pipe_fd[2];
+    pid_t pid;
 
-	open_redirection_files0(command, pipe_fd);
-	open_redirection_files1(command, &fd, pipe_fd);
-	pid_t pid;
-	pid = fork();
-	if(pid == 0)
-	{
-		exe(command, command->command->command, envp, pipe_fd);
-	}
+    if (pipe(pipe_fd) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0)
+    {
+        // Child process
+        open_redirection_files(command, pipe_fd);
+        exe(command, command->command->command, envp, pipe_fd);
+    }
+    else
+    {
+        // Parent process
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        wait(NULL);
+    }
 }
 
 int main(int argc, char **argv, char **envp)
 {
-	if(argc == 1)
-		exit(1);
+    if (argc == 1)
+        exit(1);
     char *str = ft_strdup(argv[1]);
-	char **split = ft_split(str, '|');
-	int cnt = cnt_cmd(split);
-	int i = 0;
+    char **split = ft_split(str, '|');
+    int cnt = cnt_cmd(split);
+    int i = 0;
     t_redirection **command = (malloc(sizeof(t_redirection *) * cnt));
 
-	while(split[i])
-	{
-		initialize_redirection(&command[i]);
-		if (str == NULL)
-			exit(EXIT_FAILURE);
-		parse_redirection(split[i], command[i]);
+    while (split[i])
+    {
+        initialize_redirection(&command[i]);
+        if (str == NULL)
+            exit(EXIT_FAILURE);
+        parse_redirection(split[i], command[i]);
+        print(command[i]);
+        i++;
+    }
 
-		print(command[i]);
-		i++;
-	}
-	i = 0;
-	while(split[i])
-	{
-		pid_t pid;
-		pid = fork();
-		if(pid == 0)
-		{
-			int pipe_fd[2];
-			pipe(pipe_fd);
-			execute_left_brace(command[i], pipe_fd, envp);
-			wait(NULL);
-			close(pipe_fd[0]);
-			close(pipe_fd[1]);
+    i = 0;
+    while (split[i])
+    {
+        execute_command(command[i], envp);
+        i++;
+    }
 
-		}
-		i++;
-	}
-	i = 0;
-	while(split[i])
-	{
-		free(split[i]);
-		i++;
-	}
-	free(split);
-	free(str);
-	wait(NULL);
+    i = 0;
+    while (split[i])
+    {
+        free(split[i]);
+        i++;
+    }
+    free(split);
+    free(str);
+    wait(NULL);
     return 0;
 }
-
-//만약에 실패시 dup2사용 아니면
