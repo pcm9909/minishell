36e8e02b59195ffae984659f777b77daa17ffac5
pcm9909 <pcm9909@naver.com>
2024-08-24 16:40:38 +0900

    [Remove] free_redirection

--- free.c
+++ free.c
@@ -1,18 +1,18 @@
-#include "main.h"
+// #include "main.h"
 
-void free_redirection(t_redirection **redirection)
-{
-    free_command_list(&(*redirection)->double_left_brace->command);
-    free_command_list(&(*redirection)->left_brace->command);
-    free_command_list(&(*redirection)->double_right_brace->command);
-    free_command_list(&(*redirection)->right_brace->command);
-    free_command_list(&(*redirection)->command->command);
+// void free_redirection(t_redirection **redirection)
+// {
+//     free_command_list(&(*redirection)->double_left_brace->command);
+//     free_command_list(&(*redirection)->left_brace->command);
+//     free_command_list(&(*redirection)->double_right_brace->command);
+//     free_command_list(&(*redirection)->right_brace->command);
+//     free_command_list(&(*redirection)->command->command);
 
-    free((*redirection)->double_left_brace);
-    free((*redirection)->left_brace);
-    free((*redirection)->command);
-    free((*redirection)->double_right_brace);
-    free((*redirection)->right_brace);
-    free(*redirection);
-    *redirection = NULL;
-}
+//     free((*redirection)->double_left_brace);
+//     free((*redirection)->left_brace);
+//     free((*redirection)->command);
+//     free((*redirection)->double_right_brace);
+//     free((*redirection)->right_brace);
+//     free(*redirection);
+//     *redirection = NULL;
+// }
